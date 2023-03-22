8f66eaf9a2d3f0bca70a373b6e986cd2d0b676bb
nomarine <rafaelfijos@gmail.com>
2023-03-22 19:46:51 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using RestSharp;
+
+Console.Write("Procure um Pokémon: ");
+string? pokemon = Console.ReadLine();
+
+if(pokemon != null){
+    pokemon = pokemon.ToLower();
+    var client = new RestClient("https://pokeapi.co/api/v2");
+    var request = new RestRequest($"/pokemon/{pokemon}", Method.Get);
+    var response = client.Execute(request);
+    if(response.StatusCode == HttpStatusCode.OK) {
+        Console.WriteLine(response.Content);
+    } else {
+        Console.WriteLine("Pokémon não encontrado.");
+    }
+    
+}
+
+
+
