2493cd82ce90b50c57979a90d2d8e556ce149d1f
nomarine <rafaelfijos@gmail.com>
2023-03-23 19:15:30 -0300

    parsing of abilities, height and weight

--- Program.cs
+++ Program.cs
@@ -1,21 +1,58 @@
 ﻿using System.Net;
+using System.Text.Json;
 using RestSharp;
 
-Console.Write("Procure um Pokémon: ");
-string? pokemon = Console.ReadLine();
-
-if(pokemon != null){
-    pokemon = pokemon.ToLower();
-    var client = new RestClient("https://pokeapi.co/api/v2");
-    var request = new RestRequest($"/pokemon/{pokemon}", Method.Get);
-    var response = client.Execute(request);
-    if(response.StatusCode == HttpStatusCode.OK) {
-        Console.WriteLine(response.Content);
-    } else {
-        Console.WriteLine("Pokémon não encontrado.");
+public class App {
+    public static void Main(string[] args) {
+        Console.Write("Procure um Pokémon: ");
+        string? param = Console.ReadLine();
+        if(param != null){
+            param = param.ToLower();
+            var client = new RestClient("https://pokeapi.co/api/v2");
+            var request = new RestRequest($"/pokemon/{param}", Method.Get);
+            var response = client.Execute(request);
+            if(response.StatusCode == HttpStatusCode.OK) {
+                Mascote mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
+                Console.WriteLine($"{char.ToUpper(mascote.name[0])}{mascote.name.Substring(1)}");
+                Console.WriteLine("Altura (m): {0}", (mascote.height / 10.0));
+                Console.WriteLine("Peso (kg): {0}", (mascote.weight / 10.0));
+                Console.WriteLine("Habilidades: ");
+                foreach (Mascote.Abilities ability in mascote.abilities) {
+                        Console.WriteLine(String.Join(" ", ability.ability.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
+                } 
+                foreach (Mascote.Types.Type type in mascote.type) {
+                        Console.WriteLine(String.Join(" ", type.rype.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
+                } 
+            } else {
+                Console.WriteLine("Pokémon não encontrado.");
+            }
+        }
     }
-    
 }
 
 
+
+public class Mascote {
+    public string name { get; set; }
+    public int height { get; set; }
+    public int weight { get; set; }
+    public List<Abilities> abilities { get; set; }
+    public List<Types> types { get; set; }
+    
+    public class Abilities {
+        public Ability ability { get; set; }
+
+        public class Ability {
+            public string name { get; set; }
+        }
+    }
+
+    public class Types {
+        public Type type { get; set; }
+
+        public class Type {
+            public string name { get; set; }
+        }
+    }
+}
 
