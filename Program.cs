595b56e94b8e79be4e9ec9bb42c66b63b757a46f
nomarine <rafaelfijos@gmail.com>
2023-03-25 22:02:03 -0300

    development of the start menu and the search menu

--- Program.cs
+++ Program.cs
@@ -3,31 +3,17 @@ using System.Text.Json;
 using RestSharp;
 
 public class App {
+    public static bool isAtivo { get; set; }
+    public static string username { get; set; }
+    
     public static void Main(string[] args) {
-        Console.Write("Procure um Pokémon: ");
-        string? param = Console.ReadLine();
-        if(param != null){
-            param = param.ToLower();
-            var client = new RestClient("https://pokeapi.co/api/v2");
-            var request = new RestRequest($"/pokemon/{param}", Method.Get);
-            var response = client.Execute(request);
-            if(response.StatusCode == HttpStatusCode.OK) {
-                Mascote mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
-                Console.WriteLine($"{char.ToUpper(mascote.name[0])}{mascote.name.Substring(1)}");
-                Console.WriteLine("Altura (m): {0}", (mascote.height / 10.0));
-                Console.WriteLine("Peso (kg): {0}", (mascote.weight / 10.0));
-                Console.Write("Tipo: ");
-                foreach (Mascote.Types types in mascote.types) {
-                        Console.WriteLine(String.Join(" ", types.type.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
-                } 
-                Console.WriteLine("Habilidades: ");
-                foreach (Mascote.Abilities abilities in mascote.abilities) {
-                        Console.WriteLine(String.Join(" ", abilities.ability.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
-                } 
-            } else {
-                Console.WriteLine("Pokémon não encontrado.");
-            }
+        isAtivo = true;
+        Frontend app = new Frontend();
+        app.login();
+        do{
+            app.exibirMenu();
         }
+        while (App.isAtivo == true);
     }
 }
 
@@ -56,25 +42,89 @@ public class Mascote {
 }
 
 public class Frontend {
-    public bool isAtivo { get; set; }
+    private Backend backend = new Backend();
 
-    void getUsername() {
-        string username = Console.ReadLine();
-        if(username == string.Empty) {
-            username = "Anônimo";
-        }
-        Console.WriteLine($"username, o que você deseja?");
-        string option = Console.ReadLine();
-    }
-
-    public enum MenuOptions : byte {
+    enum MenuOptions : byte {
         Adotar = 1,
         Ver,
-        Sair
+        Sair = 0
     }
 
-    void exibirMenu() {
-
+    public void login(){
+        Console.Write("Como posso te chamar?: ");
+        App.username = backend.getUsername();
+        Console.WriteLine("\n==========================");
     }
 
+    public void exibirMenu() {
+        Console.WriteLine($"{App.username}, o que você deseja?");
+        MenuOptions[] options = (MenuOptions[])Enum.GetValues(typeof(MenuOptions));
+        foreach (MenuOptions option in options){
+            Console.WriteLine($"{(int)option} - {option}");
+        }
+        string choice = Console.ReadLine();
+        if (Enum.TryParse<MenuOptions>(choice, out var opcaoSelecionada)){
+            switch (opcaoSelecionada)
+            {
+                case MenuOptions.Adotar:
+                    pesquisarMascote();
+                    break; 
+                case MenuOptions.Sair:
+                    App.isAtivo = false;
+                    break; 
+            }
+        }
+    }
+
+    void pesquisarMascote() {
+        bool continuar = true;
+        do {
+            Console.Write("Procure um mascote pelo nome: ");
+            string param = Console.ReadLine();
+            backend.pesquisarMascote(param);
+            Console.WriteLine("\n==========================");
+            Console.WriteLine("Quer procurar por outro mascote?: ");
+            Console.WriteLine("1 - Sim");
+            Console.WriteLine("0 - Não");
+            continuar = (Console.ReadLine() == "1") ? true : false;
+            Console.WriteLine("\n==========================");
+        } while (continuar == true);
+    }
+
+}
+
+public class Backend {
+    public string getUsername() {
+        string username = Console.ReadLine();
+        if(username == string.Empty) {
+            return username = "Anônimo";
+        }
+        return username;
+    }
+
+    public void pesquisarMascote(string param = "") {
+        if(param != ""){
+            param = param.ToLower();
+            var client = new RestClient("https://pokeapi.co/api/v2");
+            var request = new RestRequest($"/pokemon/{param}", Method.Get);
+            var response = client.Execute(request);
+            if(response.StatusCode == HttpStatusCode.OK) {
+                Mascote mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
+                Console.WriteLine($"Nome: {char.ToUpper(mascote.name[0])}{mascote.name.Substring(1)}");
+                Console.WriteLine("Altura (m): {0}", (mascote.height / 10.0));
+                Console.WriteLine("Peso (kg): {0}", (mascote.weight / 10.0));
+                Console.Write("Tipo: ");
+                foreach (Mascote.Types types in mascote.types) {
+                        Console.WriteLine(String.Join(" ", types.type.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
+                } 
+                Console.WriteLine("Habilidades: ");
+                foreach (Mascote.Abilities abilities in mascote.abilities) {
+                        Console.WriteLine(String.Join(" ", abilities.ability.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
+                }
+            } else {
+                Console.WriteLine("Pokémon não encontrado.");
+            }
+        }
+
+    }
 }
