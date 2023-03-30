f2fdd685e1704acf4deb5b4222b34e9742b2593e
nomarine <rafaelfijos@gmail.com>
2023-03-29 21:47:58 -0300

    Adoption menu done; separation of view-controller refined

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ public class App {
     
     public static void Main(string[] args) {
         isActive = true;
-        Frontend app = new Frontend();
+        View app = new View();
         app.showAppTitle();
         app.login();
         do{
@@ -54,8 +54,8 @@ public class AdoptedMascote : Mascote {
     }
 }
 
-public class Frontend {
-    private Backend backend = new Backend();
+public class View {
+    private Controller controller = new Controller();
 
     Dictionary<string, string> textStates = new Dictionary<string, string>()
     {
@@ -77,6 +77,11 @@ public class Frontend {
         Voltar = 0
     }
 
+    string toCamelCase(string input) {
+        string formattedString = String.Join(" ", input.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1)));
+        return formattedString;
+    }
+
     public void showAppTitle(){
         Console.WriteLine(
         FiggleFonts.ThreePoint.Render("M A S C O T C H I"));
@@ -84,7 +89,7 @@ public class Frontend {
 
     public void login(){
         AnsiConsole.Markup($"[{textStates["prompt"]}]> Como posso te chamar? [/]");
-        App.username = backend.getUsername();
+        App.username = controller.getUsername();
         Console.WriteLine("\n==========================");
     }
 
@@ -105,8 +110,43 @@ public class Frontend {
                 case MainMenuOptions.Sair:
                     App.isActive = false;
                     break; 
+                default:
+                    AnsiConsole.MarkupLine("[red]Opção inválida.[/]");
+                    break;
             }
         }
+    }
+
+    void showAdoptionMenu(Mascote mascote) {
+        bool continuar = true;
+        do {
+            AnsiConsole.MarkupLine($"[{textStates["prompt"]}]> [{textStates["variable"]}]{App.username}[/], deseja adotar o [/][{textStates["variable"]}]{toCamelCase(mascote.name)}?[/]");
+            AdoptionMenuOptions[] options = (AdoptionMenuOptions[])Enum.GetValues(typeof(AdoptionMenuOptions));
+            foreach (AdoptionMenuOptions option in options){
+                Console.WriteLine($"{(int)option} - {option}");
+            }
+            string choice = Console.ReadLine();
+            Console.WriteLine("\n==========================");
+            if (Enum.TryParse<AdoptionMenuOptions>(choice, out var opcaoSelecionada)){
+                switch (opcaoSelecionada)
+                {
+                    case AdoptionMenuOptions.SaberMais:
+                        showMascoteDetails(mascote);
+                        break;
+                    case AdoptionMenuOptions.Adotar:
+                        AdoptedMascote adoptedMascote = controller.adoptMascote(mascote);
+                        adoptMascote(adoptedMascote);
+                        continuar = false;
+                        break; 
+                    case AdoptionMenuOptions.Voltar:
+                        continuar = false;
+                        break; 
+                    default:
+                        AnsiConsole.MarkupLine("[red]Opção inválida.[/]");
+                        break;
+                }
+            }
+        } while (continuar == true);
     }
 
     void searchMascote() {
@@ -114,7 +154,16 @@ public class Frontend {
         do {
             AnsiConsole.Markup("[blue]> Informe o nome do mascote para a gente achar ele aqui: [/]");
             string param = Console.ReadLine();
-            backend.searchMascote(param);
+            if (param != "") {
+                Mascote mascote = controller.searchMascote(param);
+                if (mascote != null) {
+                    showAdoptionMenu(mascote);
+                } else {
+                    AnsiConsole.MarkupLine("[red]Mascote não localizado.[/]");
+                }
+            } else {
+                AnsiConsole.Markup("[red]Nenhum mascote informado.[/]");
+            }
             Console.WriteLine("\n==========================");
             AnsiConsole.MarkupLine($"[{textStates["prompt"]}]>[/] [{textStates["variable"]}]{App.username}[/][{textStates["prompt"]}], quer continuar a procurar por mascotes?: [/]");
             Console.WriteLine("1 - Sim");
@@ -124,35 +173,26 @@ public class Frontend {
         } while (continuar == true);
     }
 
-    void showAdoptionMenu() {
-        AnsiConsole.MarkupLine($"[{textStates["prompt"]}]> [{textStates["variable"]}]{App.username}[/], deseja continuar a adoção?[/]");
-        AdoptionMenuOptions[] options = (AdoptionMenuOptions[])Enum.GetValues(typeof(AdoptionMenuOptions));
-        foreach (AdoptionMenuOptions option in options){
-            Console.WriteLine($"{(int)option} - {option}");
-        }
-        string choice = Console.ReadLine();
-        Console.WriteLine("\n==========================");
-        if (Enum.TryParse<AdoptionMenuOptions>(choice, out var opcaoSelecionada)){
-            switch (opcaoSelecionada)
-            {
-                case AdoptionMenuOptions.SaberMais:
-                    searchMascote();
-                    break; 
-                case AdoptionMenuOptions.Adotar:
-                    //adoptMascote(adoptedMascote);
-                    break; 
-                case AdoptionMenuOptions.Voltar:
-                    break; 
-            }
+    void showMascoteDetails(Mascote mascote){
+        Console.WriteLine($"Nome: {char.ToUpper(mascote.name[0])}{mascote.name.Substring(1)}");
+        Console.WriteLine("Altura (m): {0}", (mascote.height / 10.0));
+        Console.WriteLine("Peso (kg): {0}", (mascote.weight / 10.0));
+        Console.Write("Tipo: ");
+        foreach (Mascote.Types types in mascote.types) {
+                Console.WriteLine(toCamelCase(types.type.name));
+        } 
+        Console.WriteLine("Habilidades: ");
+        foreach (Mascote.Abilities abilities in mascote.abilities) {
+                Console.WriteLine(toCamelCase(abilities.ability.name));
         }
     }
 
     void adoptMascote(AdoptedMascote adoptedMascote) {
-        //AnsiConsole.MarkupLine($"[{textStates["prompt"]}]>[/] [{textStates["variable"]}]{App.username}[/][{textStates["prompt"]}], você adotou {mascoteAdotado}! Agora é só aguardar o ovo chocar! [/]");
+        AnsiConsole.MarkupLine($"[{textStates["prompt"]}]>[/] [{textStates["variable"]}]{App.username}[/][{textStates["prompt"]}], você adotou [{textStates["variable"]}]{toCamelCase(adoptedMascote.name)}[/]! Agora é só aguardar o ovo chocar! [/]");
     }
 }
 
-public class Backend {
+public class Controller {
     public string getUsername() {
         string username = Console.ReadLine();
         if(username == string.Empty) {
@@ -161,37 +201,22 @@ public class Backend {
         return username;
     }
 
-    public void searchMascote(string param = "") {
-        if(param != ""){
-            param = param.ToLower();
-            var client = new RestClient("https://pokeapi.co/api/v2");
-            var request = new RestRequest($"/pokemon/{param}", Method.Get);
-            var response = client.Execute(request);
-            if(response.StatusCode == HttpStatusCode.OK) {
-                Mascote mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
-                Console.WriteLine($"Nome: {char.ToUpper(mascote.name[0])}{mascote.name.Substring(1)}");
-                Console.WriteLine("Altura (m): {0}", (mascote.height / 10.0));
-                Console.WriteLine("Peso (kg): {0}", (mascote.weight / 10.0));
-                Console.Write("Tipo: ");
-                foreach (Mascote.Types types in mascote.types) {
-                        Console.WriteLine(String.Join(" ", types.type.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
-                } 
-                Console.WriteLine("Habilidades: ");
-                foreach (Mascote.Abilities abilities in mascote.abilities) {
-                        Console.WriteLine(String.Join(" ", abilities.ability.name.Split('-').Select(p => p.Substring(0,1).ToUpper() + p.Substring(1))));
-                }
-
-            } else {
-                Console.WriteLine("Não identificamos esse mascote");
-            }
+    public Mascote? searchMascote(string param) {
+        param = param.ToLower();
+        var client = new RestClient("https://pokeapi.co/api/v2");
+        var request = new RestRequest($"/pokemon/{param}", Method.Get);
+        var response = client.Execute(request);
+        if(response.StatusCode == HttpStatusCode.OK) {
+            Mascote mascote = JsonSerializer.Deserialize<Mascote>(response.Content);
+            return mascote;
         } else {
-            Console.WriteLine("Mascote não informado");
+            return null;
         }
     }
 
-    public AdoptedMascote adotarMascote(Mascote mascote) {
-        AdoptedMascote mascoteAdotado = new AdoptedMascote(mascote);
-
-        return mascoteAdotado;
+    public AdoptedMascote adoptMascote(Mascote mascote) {
+        AdoptedMascote adoptedMascote = new AdoptedMascote(mascote);
+        return adoptedMascote;
     }
-}
+
+}    
